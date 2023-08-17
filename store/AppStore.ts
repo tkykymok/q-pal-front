686e2d7804014e1ab009bb6a7d3054a0079df640
tkykymok <tkykymok2@icloud.com>
2023-08-17 18:48:01 +0900

    グローバルstateを追加

--- store/AppStore.ts
+++ store/AppStore.ts
@@ -1,15 +1,36 @@
 import create, { SetState, GetState } from 'zustand';
+import { CardStatus } from '@/constant/CardStatus';
+import Status = CardStatus.Status;
+import PENDING = CardStatus.PENDING;
+import CANCELED = CardStatus.CANCELED;
 
-// Define a state type
 type AppStore = {
   isLoading: boolean;
   setLoading: (loadingState: boolean) => void;
+  showPendingColumn: boolean;
+  showCancelColumn: boolean;
+  setShowColumn: (status: Status) => void;
 };
 
-// Define your store
-const useAppStore = create<AppStore>((set: SetState<AppStore>, get: GetState<AppStore>) => ({
-  isLoading: false,
-  setLoading: (loadingState: boolean) => set({ isLoading: loadingState }),
-}));
+const useAppStore = create<AppStore>(
+  (set: SetState<AppStore>, get: GetState<AppStore>) => ({
+    isLoading: false,
+    setLoading: (loadingState: boolean) => set({ isLoading: loadingState }),
+    showPendingColumn: false,
+    showCancelColumn: false,
+    setShowColumn: (status: Status) => {
+      switch (status) {
+        case PENDING:
+          set((prev) => ({ showPendingColumn: !prev.showPendingColumn }));
+          break;
+        case CANCELED:
+          set((prev) => ({ showCancelColumn: !prev.showCancelColumn }));
+          break;
+        default:
+          break;
+      }
+    },
+  })
+);
 
 export default useAppStore;
